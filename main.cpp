94257f46f84bb5a5bd347f8704abe77780e28f54
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-11-13 11:48:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+    int n=0;
+    cin>>n;
+    if(n>=2){
+        int risultato=1,primo=1,secondo=1;
+        for(int i=1;i<=n;i++){
+            risultato=primo+secondo;
+            primo=secondo;
+            secondo=risultato;
+            cout<<risultato<<endl;
+        }
+    }else{
+        cout<<"errore"<<endl;
+    }
 }
